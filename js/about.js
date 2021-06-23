ea6e51abc67648b702a2b21baf21cc5e1d7be9fd
chelinchan24 <me@chelinchan.com>
2021-06-23 23:39:34 +0800

    2020.2.7

--- js/about.js
+++ js/about.js
@@ -6,20 +6,31 @@ $(document).on('ready', function () {
     faqInit()
 })
 
+var timer;
+
 $(window).resize(function () {
+    clearTimeout(timer);
+    faqArticleDec.attr('style','position:absolute')
     faqArticleDec.each(function() {
-        $(this).attr('style','')
+        faqArticle.removeClass('收合')
+        faqInit()
     })
+    timer = setTimeout(function () {faqArticleDec.attr('style','position:unset')}, 500)
 })
 
 function faqInit() {
     faqArticleDec.each(function() {
         $(this).css('height', $(this).height())
     })
+    faqArticle.removeClass('展開')
     faqArticle.addClass('收合')
 }
 
 $(document).on('click', '.關於-問與答-條目', function (e) {
+    if($(this).offsetTop < 0) {
+        console.log($(this).offset.top)
+        $(this).offset({top: 24})
+    }
     if ($(this).hasClass('展開')) {
         $(this).removeClass('展開')
         $(this).addClass('收合')
